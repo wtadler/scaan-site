4aaf78a1b9fb5209c84f962c8ec7722c38af9372
Will Adler <will.adler@nyu.edu>
2017-01-17 00:05:23 -0500

    center ongoing project, minor edits

--- index.php
+++ index.php
@@ -1,64 +1,65 @@
 <?php require_once "header.html"; ?>
 
 <body>
-    <div class="jumbotron">
-        <div class="container">
-          <nav class="navbar navbar-toggleable-xl navbar-inverse">
-           <img class="navbar-brand d-inline-block align-top" src="img/logo_notbold_white.svg" alt="">
-           <span class="nav-link nav-item active">Home</span>
-           <a class="nav-link nav-item" href="members.php">Members</a>
-       </nav>
+	<div class="jumbotron">
+		<div class="container">
+			<nav class="navbar navbar-toggleable-xl navbar-inverse">
+			 <img class="navbar-brand d-inline-block align-top" src="img/logo_notbold_white.svg" alt="">
+			 <span class="nav-link nav-item active">Home</span>
+			 <a class="nav-link nav-item" href="members">Members</a>
+		 </nav>
 
-       <h1 class="title">Scientist Action and Advocacy Network</h1>
-       <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
-   </div>  
+		 <h1 class="title">Scientist Action and Advocacy Network</h1>
+		 <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
+	 </div>  
+ </div>
+
+ <div class="container">
+	<div class="row">
+		<div class="col-md">
+			<h1 class="title">Action</h1>
+			<h3><em>Pro bono</em> science</h3>
+			<p>Our members are doctoral students, PhD-holding research scientists, and professors who donate their skilled labor and expertise to our partner organizations. We can help formulate questions that can be answered with data, analyze pre-existing data, and figure out how to act on the results. We can also serve as guides to particular domains of scientific knowledge.</p>
+
+
+
+		</div>
+
+		<div class="col-md">
+			<h1 class="title">Advocacy</h1>
+			<h3>Lobbying and outreach</h3>
+			<p>We organize actions to encourage our elected officials to support both evidence-based policies and scientific discovery. We also promote candidates with pro-science platforms.</p>
+		</div>
+	</div>
+
+	<div class="row">
+	<div class="col-md-10 offset-md-1">
+				<div id="accordion" role="tablist" aria-multiselectable="true">
+				<div class="card topcard bottomcard">
+					<div class="card-header" role="tab" id="headingOne">
+						<h4 class="mb-0">
+							
+							Ongoing project
+							
+						</h4>
+					</div>
+
+					<div id="rta" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+						<div class="card-block">
+							<div class="media">
+								<a href="http://raisetheageny.com/"><img class="d-flex align-self-start mr-3 projectpartnerlogo" src="img/raisetheage_square.png"></a>
+								<div class="media-body">
+									<p>Currently, New York State and North Carolina are the only two states that prosecute all 16- and 17-year-olds as adults, incarcerating them in adult jails and prisons, severely affecting their re-entry, rehabilitation, and social development. <a href="http://raisetheageny.com/">Raise the Age NY</a> is a campaign that supports a comprehensive approach to raising the age of criminal responsibility in New York State.</p>
+									<p>At ScAAN, we are compiling scientific evidence for Raise the Age NY on the social stress and trauma of adult detention centers, and on the effectiveness of rehabilitation through means other than adult detention centers. We are also working with Raise the Age NY to produce infographics appropriate for public communications.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+</div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-7">
-            <h1 class="title">Action</h1>
-            <h3><em>Pro bono</em> science</h3>
-            <p>Our members are doctoral students, PhD-holding research scientists, and professors who donate their skilled labor and expertise to our partner organizations. We can help formulate questions that can be answered with data, analyze pre-existing data, and figure out how to act on the results. We can also serve as guides to particular domains of scientific knowledge.</p>
 
-            <div id="accordion" role="tablist" aria-multiselectable="true">
-              <div class="card topcard">
-                <div class="card-header" role="tab" id="headingOne">
-                  <h5 class="mb-0">
-                    
-                      Current project: <a data-toggle="collapse" href="#rta" aria-expanded="false" aria-controls="rta">Raise the Age New York
-                  </a>
-              </h5>
-          </div>
-
-          <div id="rta" class="collapse" role="tabpanel" aria-labelledby="headingOne">
-              <div class="card-block">
-                <div class="media">
-                    <img class="d-flex align-self-start mr-3" src="img/raisetheage_square.png" width=100>
-                    <div class="media-body">
-                        <p>Currently, New York State and North Carolina are the only two states that prosecute all 16- and 17-year-olds as adults, incarcerating them in adult jails and prisons, severely affecting their re-entry, rehabilitation, and social development. Raise the Age NY is a campaign that supports a comprehensive approach to raising the age of criminal responsibility in New York State.</p>
-                        <p>At ScAAN, we are compiling scientific evidence for Raise the Age NY that suggests lasting negative effects caused by the social stress and trauma of adult detention centers. Furthermore, juveniles are particularly responsive to rehabilitation and habit reformation through means other than adult detention centers. We are also working with Raise the Age NY to produce infographics appropriate for public communications.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-
-</div>
-
-
-</div>
-
-<div class="col-md">
-    <h1 class="title">Advocacy</h1>
-    <h3>Lobbying and outreach</h3>
-    <p>We organize actions to encourage our elected officials to support both evidence-based policies and scientific discovery. We also promote candidates with pro-science platforms.</p>
-</div>
-</div>
-
-
-<hr>
-<?php require_once "footer.html"; ?>
+	<hr>
+	<?php require_once "footer.html"; ?>
