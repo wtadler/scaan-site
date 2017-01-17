b86960b13fcf0739ed79010c9b9137a0dcc26f92
Will Adler <will.adler@nyu.edu>
2017-01-16 23:06:31 -0500

    php, bios, project accordion

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php require_once "header.html"; ?>
+
+<body>
+    <div class="jumbotron">
+        <div class="container">
+          <nav class="navbar navbar-toggleable-xl navbar-inverse">
+           <img class="navbar-brand d-inline-block align-top" src="img/logo_notbold_white.svg" alt="">
+           <span class="nav-link nav-item active">Home</span>
+           <a class="nav-link nav-item" href="members.php">Members</a>
+       </nav>
+
+       <h1 class="title">Scientist Action and Advocacy Network</h1>
+       <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
+   </div>  
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-7">
+            <h1 class="title">Action</h1>
+            <h3><em>Pro bono</em> science</h3>
+            <p>Our members are doctoral students, PhD-holding research scientists, and professors who donate their skilled labor and expertise to our partner organizations. We can help formulate questions that can be answered with data, analyze pre-existing data, and figure out how to act on the results. We can also serve as guides to particular domains of scientific knowledge.</p>
+
+            <h4>Ongoing projects</h4>
+            <div id="accordion" role="tablist" aria-multiselectable="true">
+              <div class="card topcard">
+                <div class="card-header" role="tab" id="headingOne">
+                  <h5 class="mb-0">
+                    <a data-toggle="collapse" href="#rta" aria-expanded="false" aria-controls="rta">
+                      Raise the Age New York
+                  </a>
+              </h5>
+          </div>
+
+          <div id="rta" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+              <div class="card-block">
+                <div class="media">
+                    <img class="d-flex align-self-start mr-3" src="img/raisetheage_square.png" width=100>
+                    <div class="media-body">
+                        <p>Currently, New York State and North Carolina are the only two states that prosecute all 16- and 17-year-olds as adults, incarcerating them in adult jails and prisons, severely affecting their re-entry, rehabilitation, and social development. Raise the Age NY is a campaign that supports a comprehensive approach to raising the age of criminal responsibility in New York State.</p>
+                        <p>At ScAAN, we are compiling scientific evidence for Raise the Age NY that suggests lasting negative effects caused by the social stress and trauma of adult detention centers. Furthermore, juveniles are particularly responsive to rehabilitation and habit reformation through means other than adult detention centers. We are also working with Raise the Age NY to produce infographics appropriate for public communications.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card bottomcard">
+        <div class="card-header" role="tab" id="headingOne">
+          <h5 class="mb-0">
+              <a data-toggle="collapse"  href="#ny4p" aria-expanded="false" aria-controls="ny4p">
+                  New Yorkers for Parks
+              </a>
+          </h5>
+      </div>
+
+      <div id="ny4p" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+          <div class="card-block">
+              <div class="media">
+                <img class="d-flex align-self-start mr-3" src="img/ny4p.svg" width=100>
+                <div class="media-body">
+                    <p>Fusce cursus justo risus, laoreet aliquam enim pharetra et. Donec non lectus volutpat risus vulputate interdum. Duis convallis in mi sed volutpat. Aliquam diam ex, ornare nec augue nec, efficitur vehicula leo. Fusce ornare at dolor vel bibendum.</p>
+                    <p>Pellentesque at ante eget ligula mattis scelerisque eu non libero. Sed fringilla tempor est, ut ornare ipsum ultrices vel. Mauris venenatis risus molestie nulla congue, quis placerat sem porta. Pellentesque non ex in lacus tincidunt tincidunt nec eget neque. Duis venenatis sed odio eu iaculis. Donec vitae pellentesque urna. </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+
+</div>
+
+
+</div>
+
+<div class="col-md">
+    <h1 class="title">Advocacy</h1>
+    <h3>Lobbying and outreach</h3>
+    <p>We organize actions to encourage our elected officials to support both evidence-based policies and scientific discovery. We also promote candidates with pro-science platforms.</p>
+</div>
+</div>
+
+
+<hr>
+<?php require_once "footer.html"; ?>
---
+++ members.php
@@ -0,0 +1,124 @@
+<?php require_once "header.html"; ?>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+          <nav class="navbar navbar-toggleable-xl navbar-inverse">
+             <img class="navbar-brand d-inline-block align-top" src="img/logo_notbold_white.svg" alt="">
+             <a class="nav-link nav-item" href="index.php">Home</a>
+             <span class="nav-link nav-item active">Members</span>
+         </nav>
+
+         <h1 class="title">Scientist Action and Advocacy Network</h1>
+         <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
+     </div>  
+ </div>
+
+ <div class="container">
+    <div class="row">
+        <div class="col-md">
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/will2_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        <a href="http://wtadler.com/">William T. Adler</a>
+                        <a href="mailto:will@wtadler.com" title="will@wtadler.com">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+                    </h4>
+                    <p>Will is the founder of ScAAN and a Predoctoral Fellow in the Center for Neural Science at NYU. He is passionate about building a network of volunteer scientists, and founded ScAAN to bridge the gap between scientists and organizations working for the public good. Will is particularly interested in issues relating to criminal justice and electoral reform.</p>
+                </div>
+            </div>
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/weiji_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        Dr. Wei Ji Ma, PhD
+                        <a href="mailto:weijima@nyu.edu" title="weijima@nyu.edu">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+                    </h4>
+                    <p>Wei Ji is Associate Professor of Neural Science and Psychology at NYU, where <a href="http://www.cns.nyu.edu/malab/">his research group</a> studies human decision-making, perception, and working memory. He is the first author of an upcoming textbook on mathematical models of behavior. Wei Ji has long-standing interests in education policy, non-profit management, and science outreach. He is the co-founder and Chairman of the Board of the <a href="http://ruralchina.org">Rural China Education Foundation</a>, a 501(c)(3) non-profit organization which aims to improve the quality of primary education in rural China through student-centered teaching methods, community-based content, and sustainable teacher professional development.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/jenn_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">Jenn Laura Lee</h4>
+                    <p>Jenn is a Predoctoral Fellow in the Center for Neural Science at NYU. She is spearheading ScAAN's project with Raise the Age NY, providing scientific support for legislation that would raise the age at which juveniles are tried as adults in New York State. She is interested in working with public advocacy groups in general.</p>
+                </div>
+            </div>
+
+
+        </div>
+        
+        <div class="col-md">
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/silvia2_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        Dr. Silvia Lopez-Guzman, MD
+                        <a href="mailto:slg471@nyu.edu" title="slg471@nyu.edu">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+
+                    </h4>
+                    <p>Silvia is a Predoctoral Fellow at the Center for Neural Science at NYU, where she studies the connection between drug addiction and decision-making. She is interested in supporting advocacy groups fighting against juvenile incarceration, racial profiling, and the criminalization of drug addiction. She is working on an initiative to connect public defenders with scientific expert witnesses.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/milenna_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">Milenna van Dijk, MSc
+                    <a href="mailto:milenna.vandijk@gmail.com" title="milenna.vandijk@gmail.com">
+                        <img id="email" class="svg" src="img/email.svg">
+                    </a>
+                    </h4>
+
+                    <p>Milenna is a PhD Candidate in Neuroscience at NYU. She is working on ScAAN’s Raise the Age project, and is also interested in supporting campaigns that work to restrict solitary confinement. Specifically, she is gathering research findings that demonstrate the trauma and long-term negative consequences of solitary confinement of juveniles and of juveniles being held in adult jails and prisons.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/leana_sq.jpg">
+                <div class="media-body">
+                    <h4 class="mt-0">Leana King</h4>
+                    <p>Leana is a Neural Science student and researcher at NYU, and is interested in how good design can produce effective advocacy.</p>
+                </div>
+            </div>
+
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">David Halpern</h4>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">William F. Broderick</h4>
+                </div>
+            </div>
+            
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">Owen Marschall</h4>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">Andra Mihali</h4>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <hr>
+    <?php require_once "footer.html"; ?>
