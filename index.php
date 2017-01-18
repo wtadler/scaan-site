6b4f4412fdf55c5828192656c281baa93e95c233
Will Adler <will.adler@nyu.edu>
2017-01-18 02:47:29 -0500

    single page

--- index.php
+++ index.php
@@ -1,21 +1,47 @@
-<?php require_once "header.html"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.">
+        <meta name="author" content="">
+        <meta name="google-site-verification" content="zF32yFGiPnamVn1rhcrc311deNarsiAmquLuY1TY7mg" />
+        <link rel="icon" href="img/favicaan.ico" type="image/x-icon">
 
-<body>
-	<div class="jumbotron">
-		<div class="container">
-			<nav class="navbar navbar-toggleable-xl navbar-inverse">
-			 <img class="navbar-brand d-inline-block align-top" src="img/logo_notbold_white.svg" alt="">
-			 <span class="nav-link nav-item active">Home</span>
-			 <a class="nav-link nav-item" href="members">Members</a>
-		 </nav>
+        <title>Scientist Action and Advocacy Network | ScAAN</title>
 
-		 <h1 class="title">Scientist Action and Advocacy Network</h1>
-		 <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
-	 </div>  
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="scaan.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i" rel="stylesheet">
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-89937713-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="svg_to_inline.js"></script>  
+    </head>
+
+    <body>
+    <div class="jumbotron">
+        <div class="container">
+         <h1 class="title"><img class="title" src="img/logo_notbold_white.svg" alt="">
+
+         Scientist Action and Advocacy Network</h1>
+         <p>We are a New York-based group of scientists who partner with partisan and non-partisan organizations that are creating positive social change.</p>
+     </div>  
  </div>
-
  <div class="container">
-	<div class="row">
+	<div class="row" id="action">
 		<div class="col-md">
 			<h1 class="title">Action</h1>
 			<h3><em>Pro bono</em> science</h3>
@@ -60,6 +86,191 @@
 </div>
 </div>
 
+<hr>
+
+
+    <div id="members" class="row">
+        <div class="col-md">
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/will2_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        <a href="http://wtadler.com/" title="wtadler.com">William T. Adler</a>
+                        <a href="mailto:will@wtadler.com" title="will@wtadler.com">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+                    </h4>
+                    <p>Will is the founder of ScAAN and a predoctoral fellow in the Center for Neural Science at NYU. He is passionate about building a network of volunteer scientists, and founded ScAAN to bridge the gap between scientists and organizations working for the public good. Will is particularly interested in issues relating to criminal justice and electoral reform.</p>
+                </div>
+            </div>
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/weiji_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        Wei Ji Ma, PhD
+                        <a href="mailto:weijima@nyu.edu" title="weijima@nyu.edu">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+                    </h4>
+                    <p>Wei Ji is an associate professor of neural science and psychology at NYU, where <a href="http://www.cns.nyu.edu/malab/" title="Ma Lab">his research group</a> studies human decision-making, perception, and working memory. He is the first author of an upcoming textbook on mathematical models of behavior. Wei Ji has long-standing interests in education policy, non-profit management, and science outreach. He is the co-founder and chairman of the board of the <a href="http://ruralchina.org" title="Rural China Education Foundation">Rural China Education Foundation</a>, a 501(c)(3) non-profit organization which aims to improve the quality of primary education in rural China through student-centered teaching methods, community-based content, and sustainable teacher professional development.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/jenn_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">Jenn Laura Lee
+                    <a href="mailto:jenn.laura.lee@gmail.com" title="jenn.laura.lee@gmail.com">
+                        <img id="email" class="svg" src="img/email.svg">
+                    </a></h4>
+
+                    <p>Jenn is a predoctoral fellow in the Center for Neural Science at NYU. She is spearheading ScAAN's project with Raise the Age NY, providing scientific support for legislation that would raise the age at which juveniles are tried as adults in New York State. She is interested in working with public advocacy groups in general.</p>
+                </div>
+            </div>
+
+
+        </div>
+        
+        <div class="col-md">
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/silvia2_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">
+                        Silvia Lopez-Guzman, MD
+                        <a href="mailto:slg471@nyu.edu" title="slg471@nyu.edu">
+                            <img id="email" class="svg" src="img/email.svg">
+                        </a>
+
+                    </h4>
+                    <p>Silvia is a predoctoral fellow at the Center for Neural Science at NYU, where she studies the connection between drug addiction and decision-making. She is interested in supporting advocacy groups fighting against juvenile incarceration, racial profiling, and the criminalization of drug addiction. She is working on an initiative to connect public defenders with scientific expert witnesses.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/milenna_sq.jpg">
+                <div class="media-body bio">
+                    <h4 class="mt-0">Milenna van Dijk, MSc
+                    <a href="mailto:milenna.vandijk@gmail.com" title="milenna.vandijk@gmail.com">
+                        <img id="email" class="svg" src="img/email.svg">
+                    </a>
+                    </h4>
+
+                    <p>Milenna is a predoctoral fellow in neuroscience at the NYU School of Medicine. She is working on ScAAN’s Raise the Age project, and is also interested in supporting campaigns that work to restrict solitary confinement. Specifically, she is gathering research findings that demonstrate the trauma and long-term negative consequences of solitary confinement of juveniles and of juveniles being held in adult jails and prisons.</p>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/leana_sq.jpg">
+                <div class="media-body">
+                    <h4 class="mt-0">Leana King
+                    <a href="mailto:lek338@nyu.edu" title="lek338@nyu.edu">
+                        <img id="email" class="svg" src="img/email.svg">
+                    </a></h4>
+
+                    <p>Leana is a neural science student and researcher at NYU, and is interested in how good design can produce effective advocacy.</p>
+                </div>
+            </div>
+
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">David Halpern</h4>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">William F. Broderick</h4>
+                </div>
+            </div>
+            
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">Owen Marschall</h4>
+                </div>
+            </div>
+
+            <div class="media member">
+                <img class="d-flex align-self-start mr-3 rounded headshot" src="headshots/px.png" height=1>
+                <div class="media-body">
+                    <h4 class="mt-0">Andra Mihali</h4>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
 
 	<hr>
-	<?php require_once "footer.html"; ?>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <h3>Contact us</h3>
+        Email us at <a href="mailto:info@scaan.net">info@scaan.net</a> if you are:
+        <ul>
+            <li>a scientist who wants to get involved</li>
+            <li>an organization that is interested in partnering with us on a project</li>
+            <li>a candidate that is interested in our support</li>
+        </ul>
+
+
+                <!-- <form id="contact-form" method="post" action="contact.php" role="form">
+
+                    <div class="messages"></div>
+
+                    <div class="controls">
+
+                        <div class="form-group">
+                            <input id="form_name" type="text" name="name" class="form-control" placeholder="Name" required="required" data-error="Your name is required.">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group">
+                            <input id="form_email" type="email" name="email" class="form-control" placeholder="Email" required="required" data-error="A valid email is required.">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group">
+                            <textarea id="form_message" name="message" class="form-control" placeholder="Message" rows="4" required="required" data-error="A message is required."></textarea>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        
+                        <input type="submit" class="btn btn-success btn-send" value="Send message">
+
+                    </div>
+
+                </form> -->
+
+            </div>
+
+        </div>
+        <hr>
+
+
+        <footer>
+            <p>&copy; Scientist Action and Advocacy Network 2017</p>
+        </footer>
+
+
+    </div> <!-- /.container-->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- contact form JavaScript -->
+<!--     <script src="contactvalidator.js"></script>
+    <script src="contact.js"></script> -->
+</body>
+</html>
