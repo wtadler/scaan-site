ba7727e4a63fa2223683a42106a95e08913e740d
Will Adler <will.adler@nyu.edu>
2017-07-09 14:07:04 -0400

    turn loop back on

--- scripts/swiper_options.js
+++ scripts/swiper_options.js
@@ -1,6 +1,6 @@
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-    loop: false,
+    loop: true,
     // autoplay: 2000,
     // If we need pagination
     pagination: '.swiper-pagination',
